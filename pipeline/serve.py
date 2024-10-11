864ea13a6cc843065e5c451e8a5f7f8202aa1a58
Adam Blanchard <ablanchard.it@gmail.com>
2024-10-11 20:28:39 +0100

    Migrated  to plotly

--- pipeline/serve.py
+++ pipeline/serve.py
@@ -1,19 +1,12 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
 import pipeline.utils as utils
 
-def plot_rainfall(df: pd.DataFrame):
-    df.plot(kind='line', y='precipitation')
-    plt.show()
+RAINFALL_PARQUET = f'{utils.CONFIG["data_dir"]}/{utils.CONFIG["staging_folder"]}/{utils.CONFIG["rainfall_staging_file_name"]}.parquet'
 
 def run_serve(*, verbose: bool = False):
-    df = pd.read_parquet(f'{utils.CONFIG["data_dir"]}/{utils.CONFIG["staging_folder"]}/{utils.CONFIG["rainfall_staging_file_name"]}.parquet')
+    df = pd.read_parquet(RAINFALL_PARQUET)
     sorted_df = df.sort_index()
-    enhanced_df = sorted_df
-    enhanced_df['year'] = pd.DatetimeIndex(enhanced_df.index).year
-    enhanced_df['month'] = pd.DatetimeIndex(enhanced_df.index).month
-    enhanced_df['day'] = pd.DatetimeIndex(enhanced_df.index).day
-    # enhanced_df.plot(kind='bar', x=['year', 'month'], y='precipitation')
-    # plt.show()
-    
+    fig = px.line(sorted_df, x=sorted_df.index, y="precipitation", title='Daily precipitation (mm)')
+    fig.show()
     
